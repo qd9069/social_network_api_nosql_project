18d1e8683cc09e4dda752412d85503818c151083
Elaine Deng <qd9069@gmail.com>
2022-05-23 20:53:41 -0700

    Modified POST and DELETE routes for friend

--- controllers/userController.js
+++ controllers/userController.js
@@ -55,10 +55,9 @@ module.exports = {
   // POST to add a new friend to a user's friend list
   addFriend(req, res) {
     console.log("You are adding a new friend");
-    console.log(req.body);
     User.findOneAndUpdate(
       { _id: req.params.userId },
-      { $addToSet: { friends: req.body } },
+      { $addToSet: { friends:req.params.friendId } },
       { runValidators: true, new: true }
     )
       .then((user) =>
@@ -74,7 +73,7 @@ module.exports = {
   removeFriend(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
-      { $pull: { friend: { friendId: req.params.friendId } } },
+      { $pull: { friends: req.params.friendId  } },
       { runValidators: true, new: true }
     )
       .then((user) =>
